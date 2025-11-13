c242c59737b8bcdc3362d5ee0a8752f9bd913f03
jczeICS <juanzapata@ics.edu.pe>
2025-11-12 19:19:52 -0500

    mejoras en login

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import databaseConfig from './config/database.config';
 
 @Module({
@@ -20,6 +21,7 @@ import databaseConfig from './config/database.config';
         uri: process.env.URI,
       }),
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
